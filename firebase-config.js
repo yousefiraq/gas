f1d2deae95262912b701dc042b12dcfa68c08dbf
yousefiraq <50072463+yousefiraq@users.noreply.github.com>
2025-03-24 22:58:06 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBf9FQ...", // 🔑 استبدل بمفتاحك
+    apiKey: "AIzaSyBf9FQ...", // 🔴 استبدل بمفتاحك
     authDomain: "gaz-delivery-1.firebaseapp.com",
+    databaseURL: "https://gaz-delivery-1-default-rtdb.firebaseio.com/",
     projectId: "gaz-delivery-1",
     storageBucket: "gaz-delivery-1.appspot.com",
     messagingSenderId: "127631148400",
@@ -12,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const realtimeDb = getDatabase(app);
 
-export { db, collection, addDoc };
+export { db, collection, addDoc, realtimeDb, ref, onValue };
